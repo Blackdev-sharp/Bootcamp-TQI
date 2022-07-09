ed6dd27ea1c6857fbc2128c419e069bd7a389c84
Blackdev-sharp <brunofreitas.662@gmail.com>
2022-07-09 11:54:07 -0300

    Atualização

---
+++ Desafios De Codigo/Dragão.js
@@ -0,0 +1,21 @@
+const entrada = [
+    3,
+
+    7000,
+    100,
+    10000,
+]
+
+for (let i = 0; i < entrada[0]; i++){
+    nivel = entrada[i + 1]
+    if (nivel > 8000){
+        console.log("Mais de 8000!");
+    }
+    else{
+        console.log("Inseto!");
+    }  
+}
+
+
+
+
